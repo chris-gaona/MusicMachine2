5234d49fbcd27c6535018e940b8fd0670ab168dd
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-20 09:04:56 -0800

    Registers a broadcast receiver in the code instead of in the manifest

--- app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -3,6 +3,7 @@ package com.teamtreehouse.musicmachine;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private Messenger mActivityMessenger = new Messenger(new ActivityHandler(this));
     private RelativeLayout mRootLayout;
     private PlaylistAdapter mAdapter;
+
+    private NetworkConnectionReceiver mReceiver = new NetworkConnectionReceiver();
 
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
@@ -157,6 +160,21 @@ public class MainActivity extends AppCompatActivity {
             unbindService(mServiceConnection);
             mBound = false;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "App is in the foreground...");
+        IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "App is in the background...");
+        unregisterReceiver(mReceiver);
     }
 
     @Override
